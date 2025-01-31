614afde2e0c00477832f065a726be9d4f8076134
Hanz727 <oskar.rasienski@gmail.com>
2025-01-31 21:41:21 +0100

    documentation update

--- src/simulator/main.py
+++ src/simulator/main.py
@@ -5,7 +5,7 @@ from src.simulator.simulator import Simulator
 from src.simulator.solver import Solver
 
 if __name__ == "__main__":
-    simulator = Simulator(Path("mazes/maze15x10_1.png"), (15,10), (7,9), (7,0))
+    simulator = Simulator(Path("src/simulator/mazes/maze15x10_1.png"), (15,10), (7,9), (7,0))
     solver = Solver(simulator)
 
     # change mode here
