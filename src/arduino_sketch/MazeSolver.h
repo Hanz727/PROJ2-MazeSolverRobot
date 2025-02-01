2b205c95df61eb35d251adf1b3233535a125142b
Hanz727 <oskar.rasienski@gmail.com>
2025-02-01 02:18:54 +0100

    Fixed MazeSolver.h version

--- src/arduino_sketch/MazeSolver.h
+++ src/arduino_sketch/MazeSolver.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "vec2.h"
 #include <stdint.h>
+#include <vector>
+
+using matrix2d = std::vector<std::vector<int8_t>>;
 
 class MazeSolver {
 private:
@@ -11,22 +14,25 @@ private:
     const uint8_t m_MazeWidth;
     const uint8_t m_MazeHeight;
 private:
+    const uint8_t m_MazeWidthEx;
+    const uint8_t m_MazeHeightEx;
+private:
     const vec2<int> m_startPos;
     const vec2<int> m_endPos;
 private:
-    int**  const m_distanceMatrix;
-    bool** const m_wallMatrix;
+    matrix2d m_distanceMatrix;
+    matrix2d m_wallMatrix;
 private:
     void clearDistanceMatrix();
     void clearWallMatrix();
 public:
-    MazeSolver(const double wallWidth, 
-               const double cellWidth,
-               const double cellHeight, 
-               const uint8_t mazeWidth, 
-               const uint8_t mazeHeight, 
-               const vec2<int> startPos, 
-               const vec2<int> endPos
+    MazeSolver(const double wallWidth,
+        const double cellWidth,
+        const double cellHeight,
+        const uint8_t mazeWidth,
+        const uint8_t mazeHeight,
+        const vec2<int> startPos,
+        const vec2<int> endPos
     );
 
     ~MazeSolver();
@@ -34,8 +40,8 @@ public:
     vec2<double> posToCm(const vec2<double>& pos) const;
     vec2<double> cmToPos(const vec2<double>& cm) const;
 
-public: 
+public:
     vec2<int> posToPosEx(const vec2<double>& pos) const;
     vec2<double> posExToPos(const vec2<int>& posEx) const;
-    
+
 };
