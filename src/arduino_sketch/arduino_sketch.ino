4d3417838a7642474fa567311fe94147bfaf347d
Hanz727 <oskar.rasienski@gmail.com>
2025-02-11 23:53:59 +0100

    updated submodules

--- src/arduino_sketch/arduino_sketch.ino
+++ src/arduino_sketch/arduino_sketch.ino
@@ -9,14 +9,10 @@ void setup() {
   // put your setup code here, to run once:
   rangeFinder.init(pins, 3, ULTRASONIC_HC_SR04);
   Serial.begin(9600);
-  Serial.println("left,right,middle"); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   rangeFinder.update();
 
-  Serial.println(String(rangeFinder.getDistance(0)) + "," +
-                String(rangeFinder.getDistance(1)) + "," +
-                String(rangeFinder.getDistance(2)));
 }
